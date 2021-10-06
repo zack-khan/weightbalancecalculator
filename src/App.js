dec026d19b4852d634fc14823181a73bb9ba2977
Zachary Khan <zacharythekhan@gmail.com>
2021-10-05 21:17:51 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA7Lb8dwmOgZVqmRaYG50EJrZI9BsM9wTE",
+  authDomain: "weight-and-balance-calculator.firebaseapp.com",
+  projectId: "weight-and-balance-calculator",
+  storageBucket: "weight-and-balance-calculator.appspot.com",
+  messagingSenderId: "380472847102",
+  appId: "1:380472847102:web:bc6439a3ca965acf55d3af",
+  measurementId: "G-9235SWWY5Y"
+});
+
+const [user] = useAuthState(auth);
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {user ? <SignIn /> : <SignedIn />}
       </header>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  
 }
 
 export default App;
